bf2cd150d87c0ff8f4872bac8b2debf0b63e0127
Pedro italo <pedro.campos@fitbank.com.br>
2023-07-31 16:31:05 -0300

    correção de .env

--- mongo/models/connection_options/mongo_db_configs.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-mongo_db_infos={
-   "HOST" : "cluster0.vfp6e.mongodb.net",
-   "PORT" : "27017",
-   "USERNAME" : "campos2512",
-   "PASSWORD" : "campos2512",
-   "DBNAME" : "mydatabase"
-}
